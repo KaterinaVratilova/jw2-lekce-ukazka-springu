c24980c63f4d3e218ec757d806417cbff125b076
Katerina Vratilova <katerina.vratilova@gmail.com>
2022-12-12 18:53:08 +0100

    Initial commit

---
+++ src/main/java/cz/czechitas/jw2lekceukazkaspringu/Jw2LekceUkazkaSpringuApplication.java
@@ -0,0 +1,13 @@
+package cz.czechitas.jw2lekceukazkaspringu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jw2LekceUkazkaSpringuApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Jw2LekceUkazkaSpringuApplication.class, args);
+  }
+
+}
---
+++ src/test/java/cz/czechitas/jw2lekceukazkaspringu/Jw2LekceUkazkaSpringuApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.czechitas.jw2lekceukazkaspringu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jw2LekceUkazkaSpringuApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
